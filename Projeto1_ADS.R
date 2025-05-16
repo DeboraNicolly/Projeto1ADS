d3eed95b7ccc740a26f896cc31ad6db9bacde154
Débora Nicolly <148395740+DeboraNicolly@users.noreply.github.com>
2025-05-16 15:35:49 -0300

    Update Projeto1_ADS.R

--- Projeto1_ADS.R
+++ Projeto1_ADS.R
@@ -7,3 +7,13 @@ Casos_C16_UF <- read.csv("PAINEL_ONCOLOGIABR17473335525.csv", header=TRUE, sep =
 
 View(estimativa_populacao)
 View(Casos_C16_UF)
+
+#Definindo o novo nome das variáveis
+variaveis = c("Unidade da Federação", "2013", "2014", "2015", "2016", "2017", "2018", "2019", "2020", "2021")
+
+#Renomear o nome das variáveis
+colnames(estimativa_populacao) <- variaveis
+colnames(Casos_C16_UF) <- variaveis
+
+#Excluindo colunas da estimativa_polulacao - uma em branco e uma com o total por ano, o que não é desejável
+estimativa_populacao <- estimativa_populacao[-c(28, 29), ]

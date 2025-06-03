dcda25e60b51f4899ea4a32be4f2fbddcceaacb1
Débora Nicolly <148395740+DeboraNicolly@users.noreply.github.com>
2025-06-03 16:03:05 -0300

    Update Projeto1_ADS.R

--- Projeto1_ADS.R
+++ Projeto1_ADS.R
@@ -5,6 +5,8 @@ library(stringr)
 library(tidyverse)
 library (readr)
 
+#-------------------------------Pré-processamento-------------------------------
+
 #Importa os dados
 estimativa_populacao <- read.csv("ibge_cnv_poptuf152625177_20_136_208.csv", header=TRUE, sep = ";")
 Casos_C16_UF <- read.csv("PAINEL_ONCOLOGIABR17473335525.csv", header=TRUE, sep = ";")
@@ -36,7 +38,7 @@ Casos_C16_UF <- Casos_C16_UF |>
 Obitos_C16 <- Obitos_C16 |>
   mutate(`Unidade da Federação` = str_remove(`Unidade da Federação`, "^\\d+\\s+"))
 
-#Deixando os dados no formato longo
+#----------------------Deixando os dados no formato longo-----------------------
 
 #vetor por ano
 anos <- as.character(2013:2021)
@@ -57,7 +59,7 @@ Taxa_de_C16 <- casos_long |>
   left_join(obitos_long, by = c("Unidade da Federação", "ano")) |>
   left_join(pop_long,   by = c("Unidade da Federação", "ano"))
 
-# 3) Mapear estado → região, calcular taxas e selecionar colunas finais
+# 3) Mapear cada Unidade Federativa na região pertecente, calcular as taxas selecionar as colunas
 Taxa_de_C16 <- Taxa_de_C16 |>
   mutate(
     regiao = case_when(
@@ -74,7 +76,7 @@ Taxa_de_C16 <- Taxa_de_C16 |>
   ) |>
   select(`Unidade da Federação`, taxa_diagnosticos, taxa_obitos, ano, regiao)
 
-#Converter 'ano' para numérico (faça isso após o pivot_longer)
+#Converter 'ano' para numérico
 Taxa_de_C16 <- Taxa_de_C16 |>
   mutate(ano = as.numeric(ano))
 
@@ -82,8 +84,10 @@ Taxa_de_C16 <- Taxa_de_C16 |>
 summary(Taxa_de_C16)
 View(Taxa_de_C16)
 
+#------------------------Criando e analisando os gráficos-----------------------
 
-# Opção B: Gráficos separados (facetas) para cada região
+#NÃO ACHEI O 1° GRÁFICO CLARO
+# Gráficos separados para cada região
 ggplot(Taxa_de_C16 |> filter(regiao != "Brasil" & !is.na(regiao)), 
        aes(x = ano, y = taxa_diagnosticos)) +
   geom_line(color = "steelblue") +
@@ -98,6 +102,8 @@ ggplot(Taxa_de_C16 |> filter(regiao != "Brasil" & !is.na(regiao)),
   scale_x_continuous(breaks = seq(min(Taxa_de_C16$ano, na.rm = TRUE), max(Taxa_de_C16$ano, na.rm = TRUE), by = 2)) +
   theme_light(base_size = 12)
 
+
+
 # Taxa média de diagnósticos por região (2013-2021)
 media_regiao_diagnosticos <- Taxa_de_C16 |>
   filter(regiao != "Brasil" & !is.na(regiao)) |>
@@ -107,7 +113,6 @@ media_regiao_diagnosticos <- Taxa_de_C16 |>
     media_taxa_obitos = mean(taxa_obitos, na.rm = TRUE)
   ) |>
   arrange(desc(media_taxa_diagnosticos))
-
 print(media_regiao_diagnosticos)
 
 # Gráfico de barras para comparar regiões
@@ -138,6 +143,7 @@ ggplot(top_10_uf, aes(x = reorder(`Unidade da Federação`, -media_taxa_diagnost
   geom_bar(stat = "identity") +
   geom_text(aes(label = round(media_taxa_diagnosticos, 1)), vjust = -0.5, size = 3) +
   labs(title = "Top 10 UFs por Média da Taxa de Diagnósticos de C16 (2013-2021)",
+       subtitle = "Média da Taxa de Diagnósticos de Neoplasia maligna do estômago por Unidade Federativa de 2013 a 2021.",
        x = "Unidade da Federação", y = "Taxa Média de Diagnóstico (por 100.000 hab.)", fill="Região") +
   theme_classic() +
   theme(axis.text.x = element_text(angle = 45, hjust = 1)) # Rotacionar texto do eixo X
@@ -152,15 +158,15 @@ Taxa_de_C16 |>
   head(1) # Ano com maior taxa para o Brasil
 
 #______________________________
-# Exemplo para Rio Grande do Norte
+# Exemplo para o Brasil
 rn_data <- Taxa_de_C16 |> 
-  filter(`Unidade da Federação` == "Rio Grande do Norte")
+  filter(`Unidade da Federação` == "Total")
 
 ggplot(rn_data, aes(x = ano, y = taxa_diagnosticos)) +
   geom_line(color = "darkgreen") +
   geom_point(color = "darkgreen") +
   geom_smooth(method = "lm", color = "red", se = FALSE) +
-  labs(title = "Taxa de Diagnósticos de C16 - Rio Grande do Norte (2013-2021)",
+  labs(title = "Taxa de Diagnósticos de C16 - Brasil (2013-2021)",
        x = "Ano", y = "Taxa de Diagnóstico (por 100.000 hab.)") +
   scale_x_continuous(breaks = seq(min(rn_data$ano, na.rm = TRUE), max(rn_data$ano, na.rm = TRUE), by = 1)) +
   theme_minimal()
